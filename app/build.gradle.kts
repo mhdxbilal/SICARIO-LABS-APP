eb05f4dd4b7949e223d556e4d9b9ecaaf8a16b3c
mhdxbilal <166596636+mhdxbilal@users.noreply.github.com>
2026-06-16 09:07:40 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,11 @@ dependencies {
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
-  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.ui.tooling.preview
+    
+  // Add this to resolve the XML attribute linking error:
+  implementation("com.google.android.material:material:1.12.0")
+
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.material.icons.core)
   implementation(libs.androidx.compose.material.icons.extended)
