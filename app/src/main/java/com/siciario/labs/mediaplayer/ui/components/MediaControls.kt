8a9b7c052ed83f8b013166ad79fa33eb4e3d66de
mhdxbilal <166596636+mhdxbilal@users.noreply.github.com>
2026-06-16 10:21:02 +0530

    Update MediaControls.kt

--- app/src/main/java/com/siciario/labs/mediaplayer/ui/components/MediaControls.kt
+++ app/src/main/java/com/siciario/labs/mediaplayer/ui/components/MediaControls.kt
@@ -8,8 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.shape.CircleShape
 import com.sicario.labs.mediaplayer.util.StorageUtils
+import com.sicario.labs.mediaplayer.util.StorageUtils.formatDuration(...)
 
 /**
  * Media Player Controls - Reusable playback control components
